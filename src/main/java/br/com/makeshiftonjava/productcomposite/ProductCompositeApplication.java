e7ca85e413b159296f4636e8a6e731281444576e
Vinicius.saraiva Acc <vinicius.saraiva.acc@accurate.com.br>
2018-01-10 00:28:05 -0200

    Update pom to Edgware.RELEASE dependency and enable Hystrix

--- src/main/java/br/com/makeshiftonjava/productcomposite/ProductCompositeApplication.java
+++ src/main/java/br/com/makeshiftonjava/productcomposite/ProductCompositeApplication.java
@@ -3,8 +3,10 @@ package br.com.makeshiftonjava.productcomposite;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+@EnableHystrix
 @EnableDiscoveryClient
 public class ProductCompositeApplication {
 
