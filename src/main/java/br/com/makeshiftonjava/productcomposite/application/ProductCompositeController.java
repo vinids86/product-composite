229018e7c349af98bee5714e183fd7797e6dc524
Vinicius.saraiva Acc <viniciusdsaraiva@gmail.com>
2018-01-13 18:51:52 -0200

    Add Zipkin trace

--- src/main/java/br/com/makeshiftonjava/productcomposite/application/ProductCompositeController.java
+++ src/main/java/br/com/makeshiftonjava/productcomposite/application/ProductCompositeController.java
@@ -2,6 +2,8 @@ package br.com.makeshiftonjava.productcomposite.application;
 
 import br.com.makeshiftonjava.productcomposite.model.ProductAggregated;
 import br.com.makeshiftonjava.productcomposite.service.ProductAggregatedService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/product-composite")
 public class ProductCompositeController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ProductCompositeController.class);
+
     private ProductAggregatedService service;
 
     @Autowired
@@ -22,8 +26,11 @@ public class ProductCompositeController {
 
     @RequestMapping(value = "/{productId}", method = RequestMethod.GET)
     public ResponseEntity<ProductAggregated> retrieveProductComposite(@PathVariable Long productId) {
-
-        final ProductAggregated productAggregated = service.getProductAggregated(productId);
+        LOG.info("ProductComposite: product-composite/productId/" + productId);
+        final ProductAggregated productAggregated = service.getProduct(productId);
+        final ProductAggregated crossSelling = service.getCrossSelling(productId);
+        productAggregated.setProductId(crossSelling.getProductId());
+        LOG.info("returned productAggregated: " + productAggregated.getName() + " " + productAggregated.getProductId());
         return ResponseEntity.ok(productAggregated);
     }
 }
--- src/main/java/br/com/makeshiftonjava/productcomposite/service/ProductAggregatedService.java
+++ src/main/java/br/com/makeshiftonjava/productcomposite/service/ProductAggregatedService.java
@@ -1,5 +1,6 @@
 package br.com.makeshiftonjava.productcomposite.service;
 
+import br.com.makeshiftonjava.productcomposite.ProductCompositeApplication;
 import br.com.makeshiftonjava.productcomposite.model.ProductAggregated;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,13 +22,25 @@ public class ProductAggregatedService {
     }
 
     @HystrixCommand(fallbackMethod = "defaultProduct")
-    public ProductAggregated getProductAggregated(Long productId) {
-        final ServiceInstance instance = loadBalancer.choose("product-service");
-        final String url = instance.getUri().toString() + "/products/" + productId;
-        return restTemplate.getForEntity(url, ProductAggregated.class).getBody();
+    public ProductAggregated getProduct(Long productId) {
+
+        final ServiceInstance productInstance = loadBalancer.choose("product-service");
+        final String productUrl = productInstance.getUri().toString() + "/products/" + productId;
+        return restTemplate.getForEntity(productUrl, ProductAggregated.class).getBody();
+    }
+
+    @HystrixCommand(fallbackMethod = "defaultCrossSelling")
+    public ProductAggregated getCrossSelling(Long productId) {
+        final ServiceInstance crossSelingInstance = loadBalancer.choose("cross-selling-service");
+        final String crossSellingUrl = crossSelingInstance.getUri().toString() + "/cross-selling/" + productId;
+        return restTemplate.getForEntity(crossSellingUrl, ProductAggregated.class).getBody();
     }
 
     public ProductAggregated defaultProduct(Long productId) {
-        return new ProductAggregated("Fallback", 666);
+        return new ProductAggregated("Product Fallback", 0);
+    }
+
+    public ProductAggregated defaultCrossSelling(Long productId) {
+        return new ProductAggregated("", 666);
     }
 }
